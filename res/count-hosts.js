016331e92eda0a66c97b96e8482bcfb19fbf6e09
Karl Jonathan Ward <karl.j.ward@gmail.com>
2012-02-14 11:53:59 +0000

    Count more.
    Count urls pointing to clinicaltrials.gov, ncbi, wikipedia. Count
    unique "root" domains.

---
+++ res/count-hosts.js
@@ -0,0 +1,20 @@
+var mapHostsF = function() {
+  emit(this.url.root, 1)
+}
+
+var reduceHostsF = function(key, vals) {
+  var sum = 0
+  for (var i in vals) {
+    sum += vals[i]
+  }
+  return sum
+}
+
+db.runCommand({
+  mapreduce: "citations",
+  map: mapHostsF,
+  reduce: reduceHostsF,
+  query: {"url": {"$exists": true}},
+  out: {replace: "host_counts"},
+  verbose: true
+})
--- res/count-urls.js
+++ res/count-urls.js
@@ -1,25 +1,42 @@
 var mapF = function() {
-  var value = {cite_count: 1, url_count: 0, dx_url_count: 0}
-  var citation = this.to.unstructured_citation
+  var value = {cite_count: 1, other_count: 0, dx_count: 0, wiki_count: 0, ncbi_count: 0, ct_count: 0}
+  var url = this.url
 
-  if (citation.search(/http:\/\/dx\.doi\.org/) !== -1) {
-    value["dx_url_count"] = 1
-  } else if (citation.search(/https?:\/\/[^\s]+/) !== -1) {
-    value["url_count"] = 1
+  if (url !== undefined) {
+    if (url.tld === "org" && url.root === "doi" && url.sub === "dx") {
+      value["dx_count"] = 1
+    } else if (url.tld === "gov" && url.root === "nih" && url.sub === "www.ncbi.nlm") {
+      value["ncbi_count"] = 1
+    } else if (url.tld === "gov" && url.root === "clinicaltrials") {
+      value["ct_count"] = 1
+    } else if (url.root === "wikipedia") {
+      value["wiki_count"] = 1
+    } else {
+      value["other_count"] = 1
+    }
   }
   
   emit(this.context.publication_date.year, value)
 }
 
 var reduceF = function(key, vals) {
-  var citeSum = 0, urlSum = 0, dxUrlSum = 0
+  var citeSum = 0, otherSum = 0, dxSum = 0, wikiSum = 0, ncbiSum = 0, ctSum = 0
+
   for (var i in vals) {
     citeSum += vals[i]["cite_count"]
-    urlSum += vals[i]["url_count"]
-    dxUrlSum += vals[i]["dx_url_count"]
+    otherSum += vals[i]["other_count"]
+    dxSum += vals[i]["dx_count"]
+    wikiSum += vals[i]["wiki_count"]
+    ncbiSum += vals[i]["ncbi_count"]
+    ctSum += vals[i]["ct_count"]
   }
   
-  return {cite_count: citeSum, url_count: urlSum, dx_url_count: dxUrlSum}
+  return {cite_count: citeSum, 
+          other_count: otherSum, 
+          dx_count: dxSum,
+          wiki_count: wikiSum,
+          ncbi_count: ncbiSum,
+          ct_count: ctSum}
 }
 
 db.runCommand({
