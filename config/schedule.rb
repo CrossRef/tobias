8a38af91fa41c55da73a603e1c7a313192e8564a
Karl Jonathan Ward <karl.j.ward@gmail.com>
2012-09-24 09:33:01 +0100

    Change dir on cron job.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-job_type :harvest, 'DAYS=:days ACTION=:task bundle exec rake harvest_recent'
+job_type :harvest, 'cd :path && DAYS=:days ACTION=:task bundle exec rake harvest_recent'
 
 every 2.days, :at => '11:00pm' do
   harvest 'dois', :days => 2
