3fb22681bff93c00614453442dfbcbd682327a46
Karl Jonathan Ward <karl.j.ward@gmail.com>
2012-09-25 11:33:18 +0100

    Fix typo in schedule task.

--- config/index_schedule.rb
+++ config/index_schedule.rb
@@ -1,5 +1,5 @@
 job_type :index_core, 'cd :path && CONFIG=:config CORE=:task OTHER=:then_swap_with bundle exec rake index_core'
 
-very :sunday, :at => '11:00pm' do
+every :sunday, :at => '11:00pm' do
   index_core 'labs2', :then_swap_with => 'labs1'
 end
